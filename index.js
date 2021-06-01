dcae4b68f5815b71f16e6f3f3499ffa993d4136a
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-05-31 22:18:19 -0400

    plotted out readme

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require("fs");
-
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -51,14 +52,42 @@ const promptUser = () => {
             message: 'What is your email?',
         },
     ])
-}
+    };
+
+    const generateReadMe = (answers) =>
+    `badge goes here
+    ## 1.0 Description:
+    - This is a readme generator
+    ## 2.0 Table of Contents:
+    1. [Description](#1.0-Description)
+    2. [Table of Contents](#2.0-table-of-contents)
+    3. [Installation](#3.0-installation-instructions)
+    4. [Usage](#4.0-usage-information)
+    5. [Contribution](#5.0-contribution-guidelines)
+    6. [Test](#6.0-test-instructions)
+    7. [Questions](#7.0-questions)
+    8. [License](#8.0-license)
+    ## 3.0 Installation Instructions
+    - Install the thing by steps one two three
+    ## 4.0 Usage Information
+    - Usuage information
+    ## 5.0 Contribution Guidelines
+    - contribution guidelines
+    ## 6.0 Test Instructions
+    - test instructions go here
+    ## 7.0 Questions:
+    - If you have any questions my github can be found here  https://github.com/speakeasyman
+    - Alternatively you may email me at jonathan.w.nance@gmail.com
+    ## 8.0 License:
+    License goes here`;
 
 
 const init = () => {
     promptUser()
-    //   .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+      .then((answers) => writeFileAsync('test.md', generateReadMe(answers)))
       .then(() => console.log('It finished running'))
       .catch((err) => console.error(err));
   };
+
 init();
 
