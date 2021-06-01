1b03b3a4da585b666235da173e2f478ce8f1333f
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-05-31 22:47:29 -0400

    readme is generating need badges

--- index.js
+++ index.js
@@ -54,37 +54,64 @@ const promptUser = () => {
     ])
     };
 
-    const generateReadMe = (answers) =>
-    `badge goes here
-    ## 1.0 Description:
-    - This is a readme generator
-    ## 2.0 Table of Contents:
-    1. [Description](#1.0-Description)
-    2. [Table of Contents](#2.0-table-of-contents)
-    3. [Installation](#3.0-installation-instructions)
-    4. [Usage](#4.0-usage-information)
-    5. [Contribution](#5.0-contribution-guidelines)
-    6. [Test](#6.0-test-instructions)
-    7. [Questions](#7.0-questions)
-    8. [License](#8.0-license)
-    ## 3.0 Installation Instructions
-    - Install the thing by steps one two three
-    ## 4.0 Usage Information
-    - Usuage information
-    ## 5.0 Contribution Guidelines
-    - contribution guidelines
-    ## 6.0 Test Instructions
-    - test instructions go here
-    ## 7.0 Questions:
-    - If you have any questions my github can be found here  https://github.com/speakeasyman
-    - Alternatively you may email me at jonathan.w.nance@gmail.com
-    ## 8.0 License:
-    License goes here`;
+    const generateReadMe = (answers) => 
+`
+badge goes here
+        
+        
+## 1.0 Description:
 
+- ${answers.description}
+
+
+## 2.0 Table of Contents:
+1. [Description](#1.0-Description)
+2. [Table of Contents](#2.0-table-of-contents)
+3. [Installation](#3.0-installation-instructions)
+4. [Usage](#4.0-usage-information)
+5. [Contribution](#5.0-contribution-guidelines)
+6. [Test](#6.0-test-instructions)
+7. [Questions](#7.0-questions)    
+8. [License](#8.0-license)
+        
+        
+        
+## 3.0 Installation Instructions
+
+- ${answers.installation}
+
+
+## 4.0 Usage Information
+
+- ${answers.usage}
+
+
+## 5.0 Contribution Guidelines
+
+- ${answers.contribution}
+
+
+## 6.0 Test Instructions
+
+- ${answers.test}
+
+
+## 7.0 Questions:
+
+- If you have any questions my github can be found here https://github.com/${answers.gitHub}
+
+- Alternatively you may email me at ${answers.email}
+
+
+## 8.0 License:
+
+${answers.license}`;
+    
+        
 
 const init = () => {
     promptUser()
-      .then((answers) => writeFileAsync('test.md', generateReadMe(answers)))
+      .then((answers) => writeFileAsync('testReadMe.md', generateReadMe(answers)))
       .then(() => console.log('It finished running'))
       .catch((err) => console.error(err));
   };
